8d58ca44b2eeb61320894ec055e3f2747d676ac8
“yahoso” <95371561+s00790080@users.noreply.github.com>
2025-06-30 00:06:12 +0900

    test

---
+++ main/mujineki/src/main/java/com/example/mujineki/MujinekiApplication.java
@@ -0,0 +1,13 @@
+package mujineki.src.main.java.com.example.mujineki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujinekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujinekiApplication.class, args);
+	}
+
+}
